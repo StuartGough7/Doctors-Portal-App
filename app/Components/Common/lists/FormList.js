200721dc7561ad0e0a0ceff3d31121fc728dd670
StuartTbox <sgough@therapy-box.co.uk>
2019-04-16 23:25:08 +0100

    [Stable] Added iOS icon and added Sliders

--- app/Components/Common/lists/FormList.js
+++ app/Components/Common/lists/FormList.js
@@ -1,19 +1,21 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, { useState } from 'react'
 import { View, Text } from 'react-native'
 import { TextIn } from 'app/components/common'
+import Slider from '@react-native-community/slider'
 
 export const FormList = React.memo((props) => {
   const [date, setDate] = useState('')
   const [notes, setNotes] = useState('')
-  const [duration, setDuration] = useState('')
-  const [learning, setLearning] = useState('')
-  const [satisfaction, setSatisfaction] = useState('')
-  const [involvement, setInvolvement] = useState('')
+  const [duration, setDuration] = useState(0)
+  const [learning, setLearning] = useState(1)
+  const [satisfaction, setSatisfaction] = useState(1)
+  const [involvement, setInvolvement] = useState(1)
   return (
     <>
       <View style={styles.Section}>
         <View style={styles.Split1}>
-          <Text style={styles.textStyle}>Date</Text>
+          <Text style={styles.textStyle}>Date:</Text>
         </View>
         <View style={styles.Split2}>
           <TextIn
@@ -29,7 +31,7 @@ export const FormList = React.memo((props) => {
 
       <View style={styles.Section}>
         <View style={styles.Split1}>
-          <Text style={styles.textStyle}>Notes</Text>
+          <Text style={styles.textStyle}>Notes:</Text>
         </View>
         <View style={styles.Split2}>
           <TextIn
@@ -45,14 +47,18 @@ export const FormList = React.memo((props) => {
 
       <View style={styles.Section}>
         <View style={styles.Split1}>
-          <Text style={styles.textStyle}>Duration</Text>
+          <Text style={styles.textStyle}>Duration:</Text>
         </View>
         <View style={styles.Split2}>
-          <TextIn
-            wrong={false}
-            placeholder="Insert Duration"
+          <Slider
+            style={{ width: '80%', height: 40 }}
+            step
             value={duration}
-            onChangeText={(value) => {
+            minimumValue={0}
+            maximumValue={180}
+            minimumTrackTintColor="#240F48"
+            maximumTrackTintColor="rgba(160,160,160,0.6)"
+            onValueChange={(value) => {
               setDuration(value)
             }}
           />
@@ -61,14 +67,18 @@ export const FormList = React.memo((props) => {
 
       <View style={styles.Section}>
         <View style={styles.Split1}>
-          <Text style={styles.textStyle}>Learning Experience</Text>
+          <Text style={styles.textStyle}>Learning Experience:</Text>
         </View>
         <View style={styles.Split2}>
-          <TextIn
-            wrong={false}
-            placeholder="Insert Learning Experience"
+          <Slider
+            style={{ width: '80%', height: 40 }}
+            step
             value={learning}
-            onChangeText={(value) => {
+            minimumValue={1}
+            maximumValue={5}
+            minimumTrackTintColor="#240F48"
+            maximumTrackTintColor="rgba(160,160,160,0.6)"
+            onValueChange={(value) => {
               setLearning(value)
             }}
           />
@@ -77,14 +87,18 @@ export const FormList = React.memo((props) => {
 
       <View style={styles.Section}>
         <View style={styles.Split1}>
-          <Text style={styles.textStyle}>Personal Satisfaction</Text>
+          <Text style={styles.textStyle}>Personal Satisfaction:</Text>
         </View>
         <View style={styles.Split2}>
-          <TextIn
-            wrong={false}
-            placeholder="Insert Satisfaction"
+          <Slider
+            style={{ width: '80%', height: 40 }}
+            step
             value={satisfaction}
-            onChangeText={(value) => {
+            minimumValue={1}
+            maximumValue={5}
+            minimumTrackTintColor="#240F48"
+            maximumTrackTintColor="rgba(160,160,160,0.6)"
+            onValueChange={(value) => {
               setSatisfaction(value)
             }}
           />
@@ -93,14 +107,18 @@ export const FormList = React.memo((props) => {
 
       <View style={styles.Section}>
         <View style={styles.Split1}>
-          <Text style={styles.textStyle}>Involvement</Text>
+          <Text style={styles.textStyle}>Involvement:</Text>
         </View>
         <View style={styles.Split2}>
-          <TextIn
-            wrong={false}
-            placeholder="Insert Involvement"
+          <Slider
+            style={{ width: '80%', height: 40 }}
+            step
             value={involvement}
-            onChangeText={(value) => {
+            minimumValue={1}
+            maximumValue={5}
+            minimumTrackTintColor="#240F48"
+            maximumTrackTintColor="rgba(160,160,160,0.6)"
+            onValueChange={(value) => {
               setInvolvement(value)
             }}
           />
@@ -113,16 +131,20 @@ export const FormList = React.memo((props) => {
 const styles = {
   Section: {
     width: '100%',
-    height: 20,
+    height: '12%',
     flexDirection: 'row',
     borderTopWidth: 2,
     borderColor: '#fff',
   },
   Split1: {
-    flex: 0.3,
+    flex: 0.4,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   Split2: {
-    flex: 0.7,
+    flex: 0.6,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
   },
   textStyle: {
     color: '#240F48',
--- app/services/Utils.js
+++ app/services/Utils.js
@@ -1,3 +1,40 @@
 import uuidv4 from 'uuid/v4'
 
 export const generateRandomId = () => uuidv4()
+
+export const calculateDate = (timestamp) => {
+  const DatObj = new Date(parseInt(timestamp, 10))
+  const Month = _monthSwitch(DatObj.getMonth())
+  return { Date: DatObj.getDate(), Month: Month, Year: DatObj.getUTCFullYear() }
+}
+
+const _monthSwitch = (number) => {
+  switch (number) {
+    case 0:
+      return 'Jan'
+    case 1:
+      return 'Feb'
+    case 2:
+      return 'Mar'
+    case 3:
+      return 'Apr'
+    case 4:
+      return 'May'
+    case 5:
+      return 'Jun'
+    case 6:
+      return 'Jul'
+    case 7:
+      return 'Aug'
+    case 8:
+      return 'Sep'
+    case 9:
+      return 'Oct'
+    case 10:
+      return 'Nov'
+    case 11:
+      return 'Dec'
+    default:
+      return 'Jan'
+  }
+}
