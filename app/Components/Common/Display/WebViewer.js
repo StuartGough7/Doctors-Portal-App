c1c2fb8d3c00a1ef0e68448e10c8de0d1b479f99
StuartTbox <sgough@therapy-box.co.uk>
2019-04-21 22:53:10 +0100

    Fixed WebView Issues for now

--- app/Components/Common/Display/WebViewer.js
+++ app/Components/Common/Display/WebViewer.js
@@ -1,24 +1,43 @@
 import React, { useState } from 'react'
-import { View, ActivityIndicator, Dimensions } from 'react-native'
+import { View, ActivityIndicator, Dimensions, YellowBox, Alert } from 'react-native'
 import { WebView } from 'react-native-webview'
 import { Header } from 'app/components/common'
+import { navigate } from 'app/services/navigation'
 const { height, width } = Dimensions.get('window')
-export const WebViewer = () => {
-  const [LoadingVisible, SetLoadingVisible] = useState(true)
 
+YellowBox.ignoreWarnings(['Encountered'])
+
+export const WebViewer = () => {
+  const [LoadingVisible, SetLoadingVisible] = useState(false)
   return (
     <View style={styles.container}>
       <Header headerText={'Portal Online'} />
 
       {LoadingVisible && (
         <View style={styles.LoadingScreen}>
-          <ActivityIndicator size="large" style={styles.ActivityIndicatorStyle} />
+          <ActivityIndicator
+            animating={LoadingVisible}
+            size="large"
+            style={styles.ActivityIndicatorStyle}
+          />
         </View>
       )}
       <WebView
         onLoad={() => SetLoadingVisible(false)}
-        source={{ uri: 'http://www.doctorsportal.co.za/' }}
+        source={{ uri: 'https://www.doctorsportal.co.za/' }}
         style={styles.WebView}
+        onLoadStart={() => SetLoadingVisible(true)}
+        onError={(err) => {
+          if (err) {
+            Alert.alert(
+              'Connection Error',
+              'Connection Error',
+              [{ text: 'OK', onPress: () => {} }],
+              { cancelable: false }
+            )
+            navigate('HomeScreen', null)
+          }
+        }}
       />
     </View>
   )
@@ -35,11 +54,13 @@ const styles = {
     backgroundColor: '#240F48',
   },
   ActivityIndicatorStyle: {
+    color: '#fff',
     position: 'absolute',
     top: height / 2 - 100,
     left: width / 2 - 10,
   },
   WebView: {
+    overflow: 'hidden',
     height: '100%',
     width: '100%',
     backgroundColor: '#240F48',
